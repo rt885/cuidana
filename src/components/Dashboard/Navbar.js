bbfe22d64372c5bc362c52c9e6690dfed47b05bc
rt885 <ruben.t.v@hotmail.com>
2025-07-22 12:26:10 -0600

    refactor: reorganize Navbar component and update styles in globals.css

--- src/components/Dashboard/Navbar.js
+++ src/components/Dashboard/Navbar.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div>
-      <div className='flex items-center justify-between p-4 bg-gray-800 text-white'>
+    <>
+      <div className='flex items-center justify-between px-16 py-3 bg-main-background text-white'>
         <Image
           src="/images/logo.webp"
           alt="Next.js Logo"
@@ -12,33 +12,33 @@ const Navbar = () => {
           height={40}
           priority
         />
-        <div className='flex space-x-4'>
-          <Link href="/" className="text-lg">
+        <div className='flex space-x-6'>
+          <Link href="/" className="text-primary-text">
             Home
           </Link>
-          <Link href="/" className="text-lg">
+          <Link href="/" className="text-primary-text">
             Servicios
           </Link>
-          <Link href="/" className="text-lg">
+          <Link href="/" className="text-primary-text">
             Quienes Somos
           </Link>
-          <Link href="/" className="text-lg">
+          <Link href="/" className="text-primary-text">
             Testimonios
           </Link>
-          <Link href="/" className="text-lg">
+          <Link href="/" className="text-primary-text">
             Preguntas Frecuentes
           </Link>
-          <Link href="/" className="text-lg">
+          <Link href="/" className="text-primary-text">
             Contacto
           </Link>
         </div>
         <div>
-          <Link href="/" className="text-lg font-semibold">
+          <Link href="/" className="font-semibold">
             Contactanos
           </Link>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
