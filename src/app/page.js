6a58a2b37d9460ac526bb84f5ebd4e63faad870f
rt885 <ruben.t.v@hotmail.com>
2025-07-23 12:08:26 -0600

    feat: add StatsBanner component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Home/Hero";
+import StatsBanner from "@/components/Home/StatsBanner";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <StatsBanner />
     </>
   );
 }
---
+++ src/components/home/StatsBanner.js
@@ -0,0 +1,21 @@
+
+const StatsBanner = () => {
+  return (
+    <section className="bg-[#0360D9] text-white-text lg:px-[360px] lg:py-3 flex justify-between items-center gap-4">
+      <div className="flex flex-col items-center">
+        <p className="font-lato text-[64px]">24/7</p>
+        <span className="text">Servicio Disponible</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="font-lato text-[64px]">50+</p>
+        <span className="text">Profesionales de enfermería</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="font-lato text-[64px]">100+</p>
+        <span className="text">Pacientes Atendidos</span>
+      </div>
+    </section>
+  );
+}
+
+export default StatsBanner;
