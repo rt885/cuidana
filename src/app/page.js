17f1f3729d3915fc6bf832cff08f74703666d44f
rt885 <ruben.t.v@hotmail.com>
2025-07-24 15:11:12 -0600

    fix: correct import paths in Home component for consistency

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import Hero from "@/components/Home/Hero";
-import StatsBanner from "@/components/Home/StatsBanner";
-import HomeVideo from "@/components/Home/HomeVideo";
-import Services from "@/components/Home/ServicesSection";
-import TestimonialSection from "@/components/TestimonialSection";
-import Benefits from "@/components/Home/Benefits";
-import HowItWorks from "@/components/Home/HowItWorks";
-import InitialForm from "@/components/Home/InitialForm";
-import FaQSection from "@/components/Home/FaQSection";
-import CloseBanner from "@/components/CloseBanner";
+import Hero from "../components/Home/Hero";
+import StatsBanner from "../components/Home/StatsBanner";
+import HomeVideo from "../components/Home/HomeVideo";
+import Services from "../components/Home/ServicesSection";
+import TestimonialSection from "../components/TestimonialSection";
+import Benefits from "../components/Home/Benefits";
+import HowItWorks from "../components/Home/HowItWorks";
+import InitialForm from "../components/Home/InitialForm";
+import FaQSection from "../components/Home/FaQSection";
+import CloseBanner from "../components/CloseBanner";
 
 export default function Home() {
   return (
