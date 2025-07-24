605c64a3c250f8ba3d0ff3315bd94d018f59c497
rt885 <ruben.t.v@hotmail.com>
2025-07-24 15:48:10 -0600

    feat: add Swiper integration for responsive services slider and update component structure

--- src/components/home/ServicesSection.js
+++ src/components/home/ServicesSection.js
@@ -1,7 +1,14 @@
-import { servicesData } from "../../../data/serviceCards/serviceCards";
-import ServiceCard from "../ServiceCard";
+'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { servicesData } from '../../../data/serviceCards/serviceCards';
+import ServiceCard from '../ServiceCard';
 
-const ServicesSection = () => {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const ServicesSlider = () => {
   return (
     <section className="bg-accent-background">
       <div className="px-40 py-20 bg-secondary-background rounded-b-[56px]">
@@ -15,7 +22,47 @@ const ServicesSection = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-5 gap-y-5">
+          {/* Mobile: Slider, Desktop: Grid */}
+          <div className="block lg:hidden">
+            <Swiper
+              modules={[Autoplay, Pagination]}
+              spaceBetween={20}
+              slidesPerView={1.2}
+              centeredSlides={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                480: {
+                  slidesPerView: 1.5,
+                  spaceBetween: 20,
+                },
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                  centeredSlides: false,
+                },
+              }}
+              className="services-mobile-swiper"
+            >
+              {servicesData.map((service) => (
+                <SwiperSlide key={service.id}>
+                  <ServiceCard
+                    icon={service.icon}
+                    title={service.title}
+                    description={service.description}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          {/* Desktop: Grid Layout */}
+          <div className="hidden lg:grid grid-cols-4 gap-x-5 gap-y-5">
             {servicesData.map((service) => (
               <ServiceCard
                 key={service.id}
@@ -31,4 +78,4 @@ const ServicesSection = () => {
   );
 };
 
-export default ServicesSection;
+export default ServicesSlider;
